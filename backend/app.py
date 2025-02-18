f448e4d023827fce4889084bda0e103c92663699
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-02-18 11:08:53 +0300

    Adds Auth.py

--- backend/app.py
+++ backend/app.py
@@ -36,7 +36,7 @@ jwt.init_app(app)
 from views import *
 
 app.register_blueprint(user_bp)
-# app.register_blueprint(auth_bp)
+app.register_blueprint(auth_bp)
 # app.register_blueprint(red_flag_bp)
 # app.register_blueprint(intervension_bp)
 # app.register_blueprint(contact_us_bp)
