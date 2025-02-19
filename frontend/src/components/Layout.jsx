2249e12f812dfa1f232079dfaef1b1927facf7ed
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-02-19 12:34:26 +0300

    Links the frontend login & sign up with the backend

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { ToastContainer } from "react-toastify";
 
 export default function Layout() {
   return (
@@ -9,6 +10,7 @@ export default function Layout() {
       <Navbar />
       <main className="flex-grow p-8">
         <Outlet />
+        <ToastContainer/>
       </main>
       <Footer />
     </div>
