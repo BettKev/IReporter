bab561bc82a09a5e3c6c550d23626e915ded0b7d
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-02-21 21:24:16 +0300

    Creats branch

--- backend/migrations/versions/8f2d23811975_initial_migrate.py
+++ backend/migrations/versions/8f2d23811975_initial_migrate.py
@@ -1,8 +1,8 @@
-"""Initial Migration
+"""Initial Migrate
 
-Revision ID: 6990dfa39b89
+Revision ID: 8f2d23811975
 Revises: 
-Create Date: 2025-02-20 16:36:32.309778
+Create Date: 2025-02-21 21:22:39.129856
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6990dfa39b89'
+revision = '8f2d23811975'
 down_revision = None
 branch_labels = None
 depends_on = None
