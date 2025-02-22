9c521f2717a95b35c1fd306fae019a4db92f44fe
Kevin Bett <prodigy4614@gmail.com>
2025-02-22 23:14:59 +0300

    fixes main tag className

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { ToastContainer } from "react-toastify";
 
-const Layout = () => {
+export default function Layout() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-1 container mx-auto px-4 sm:px-6 lg:px-8 py-4">
+      <main className="flex-1 container mx-auto px-4 sm:px-6 lg:px-8 py-4 overflow-auto"> {/* Allow content to scroll, if necessary */}
         <Outlet />
+        <ToastContainer />
       </main>
       <Footer />
     </div>
   );
-};
-
-export default Layout;
+}
