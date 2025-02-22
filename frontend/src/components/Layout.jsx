d5ee3f2e3ef2863dc3dd34037b6ce78cbd3ce313
Kevin Bett <prodigy4614@gmail.com>
2025-02-22 23:11:05 +0300

    Adds responsiveness to layout component

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import { ToastContainer } from "react-toastify";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-export default function Layout() {
+const Layout = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-gray-100">
       <Navbar />
-      <main className="flex-grow p-8 overflow-auto"> {/* Allow content to scroll, if necessary */}
+      <main className="flex-1 container mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <Outlet />
-        <ToastContainer />
       </main>
       <Footer />
     </div>
   );
-}
+};
+
+export default Layout;
