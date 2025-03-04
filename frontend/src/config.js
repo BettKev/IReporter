a4f3a5bc31d4c93e89b8857413b47d799e9e4426
Kevin Bett <prodigy4614@gmail.com>
2025-03-04 23:55:27 +0300

    updates

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,8 +2,8 @@
 
 const config = {
 
-                // apiURL: "https://ireporter-8cyw.onrender.com",
-                apiURL: "http://127.0.0.1:5000", // Uncomment for local testing
+                apiURL: "https://ireporter-8cyw.onrender.com",
+                // apiURL: "http://127.0.0.1:5000", // Uncomment for local testing
 
                     
                 googleClientId : "899188581344-8f98569b2lni1b6nu459j42v0eio029h.apps.googleusercontent.com"
