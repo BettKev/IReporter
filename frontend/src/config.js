87df95f2b10a33e173af25973a0b07a6246584b4
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-04-14 21:26:38 +0300

    Changes config file

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@
 
 const config = {
 
-                apiURL: "https://ireporter-ozkg.onrender.com",
+                apiURL: "https://ireporter-1-1a0z.onrender.com",
                 // apiURL: "http://127.0.0.1:5000", // Uncomment for local testing
 
                     
