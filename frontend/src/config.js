602c815de5f9484657edf554aa1f1adb56d6085a
BettKev <105424930+BettKev@users.noreply.github.com>
2025-04-14 18:35:25 +0000

    Adds Comment for backend apiURL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@
 
 const config = {
 
-                apiURL: "https://ireporter-1-1a0z.onrender.com",
+                apiURL: "https://ireporter-1-1a0z.onrender.com", //Davids Acc
                 // apiURL: "http://127.0.0.1:5000", // Uncomment for local testing
 
                     
